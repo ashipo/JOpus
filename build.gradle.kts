575c5ade887fdccb62bdeba17282f92648bc86bc
ashipo <24320267+ashipo@users.noreply.github.com>
2024-10-21 23:11:10 +0300

    Update dependencies
    - Gradle to 8.10.2
    - AGP to 8.7.1
    - Kotlin to 2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.library") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.android.library") version "8.7.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
